299292d799a2cd77802c77cb5901959ea63df6cb
johnny-moses <johnny.moses3@gmail.com>
2024-09-11 15:47:37 -0500

    Update designer route and response format
    Changed the route from '/api/designer_names' to '/api/designer/' for consistency with naming conventions. Updated the response to include designer IDs along with names in a list of dictionaries.

--- routes/dashboard_routes.py
+++ routes/dashboard_routes.py
@@ -9,9 +9,11 @@ def index():
     return jsonify({'message': 'success!'})
 
 
-@dashboard_bp.route('/api/designer_names', methods=['GET'])
+@dashboard_bp.route('/api/designer/', methods=['GET'])  # I changed the route to be plural to align with convention
 def get_designer_names():
     with DatabaseSession() as session:
-        designers = session.query(Designer.designer_name).filter(Designer.designer_name.isnot(None)).all()
-        designer_names = [designer.designer_name for designer in designers]
-        return jsonify(designer_names), 200
+        designers = session.query(Designer.id, Designer.designer_name).filter(Designer.designer_name.isnot(None)).all()
+        # Create a list of dictionaries with 'id' and 'designer_name'
+        designer_data = [{"id": designer.id, "name": designer.designer_name} for designer in designers]
+        return jsonify(designer_data), 200
+
