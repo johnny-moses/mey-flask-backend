396247cc7304a5a21ecfe62047efb4159a5c4e1d
johnny-moses <johnny.moses3@gmail.com>
2024-09-11 14:25:13 -0500

    Refactor database session management
    Moved database session management code from `models/base.py` to a new module `db/database.py` for better separation of concerns. Added a new API endpoint in `routes/dashboard_routes.py` to fetch designer names using the refactored database session management.

--- routes/dashboard_routes.py
+++ routes/dashboard_routes.py
@@ -1,7 +1,17 @@
 from flask import jsonify
 from . import dashboard_bp
+from db.database import DatabaseSession
+from models.designer import Designer
 
 
 @dashboard_bp.route('/dashboard/')
 def index():
     return jsonify({'message': 'success!'})
+
+
+@dashboard_bp.route('/api/designer_names', methods=['GET'])
+def get_designer_names():
+    with DatabaseSession() as session:
+        designers = session.query(Designer.designer_name).filter(Designer.designer_name.isnot(None)).all()
+        designer_names = [designer.designer_name for designer in designers]
+        return jsonify(designer_names), 200
